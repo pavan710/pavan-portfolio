ca5849cc808fe4b54ef555e93b8703612d497d8f
Pavan_Kalyan_Penubarthi <119917966+pavan710@users.noreply.github.com>
2025-06-25 23:58:13 +0530

    Update Resume.js

--- my-app/src/components/Resume.js
+++ my-app/src/components/Resume.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Resume() {
   return (
     <div>
-        <p>View <a href='https://drive.google.com/file/d/1rx3wmsityIol_F2G7ruIscuMZiHQGMyD/view?usp=sharing'>Resume</a>.</p>   
+        <p>View <a href='https://drive.google.com/file/d/1CbpCxUGQqpaWEZdmbcanJ7MZzV-tbja5/view?usp=sharing'>Resume</a>.</p>   
     </div>
   )
 }
